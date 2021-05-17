8a5c2bdca09865f1770d86d18fe7784b14b5f095
carlosavelino <carlos.avelino2.0@gmail.com>
2021-05-17 00:37:41 -0300

    Consulta simples com query methods

--- src/main/java/com/avelino/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/avelino/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.avelino.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.avelino.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	//stsData manta a consulta 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
